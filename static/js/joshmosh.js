14bdad083e29f02331e67603061cc93554438b04
Josh Gillespie <Joshua.Neal.Gillespie@gmail.com>
2014-04-10 03:09:15 -0700

    adding projects to portfolio

--- static/js/joshmosh.js
+++ static/js/joshmosh.js
@@ -72,5 +72,34 @@ $(document).ready(function(){
 	$(function() {
 	    $( "#tabs" ).tabs();
 	  });
+
+ 	// portfolio preview list
+ 	$('.preview_list_item a').click(function(){
+ 		$(this).parent().siblings().removeClass('selected');
+ 		$(this).parent().addClass('selected');
+
+ 		var title = $(this).siblings('.project_info').attr('title');
+ 		var description = $(this).siblings('.project_info').attr('description');
+ 		var firstsrc = $(this).siblings('.project_info').attr('firstsrc');
+ 		var github = $(this).siblings('.project_info').attr('github');
+
+ 		$('.project_top').html('<h3>'+title+'</h3><p>'+description+'</p>');
+ 		$('.project_images').html("<img class='col-md-4 project_img img1' src="+firstsrc+">");
+ 		$('.carousel-inner').html("<div class='active item'><img src="+firstsrc+"></div>")
+ 		if($(this).siblings('.project_info').attr('secondsrc') != '')
+ 		{
+ 			var secondsrc = $(this).siblings('.project_info').attr('secondsrc');
+ 			$('.project_images').append("<img class='col-md-4 project_img img1' src="+secondsrc+">")
+ 			$('.carousel-inner').append("<div class='item'><img src="+secondsrc+"></div>")
+ 		}
+ 		if($(this).siblings('.project_info').attr('thirdsrc') != '')
+ 		{
+ 			var thirdsrc = $(this).siblings('.project_info').attr('thirdsrc');
+ 			$('.project_images').append("<img class='col-md-4 project_img img1' src="+thirdsrc+">")
+ 			$('.carousel-inner').append("<div class='item'><img src="+thirdsrc+"></div>")
+ 		}
+ 		$('.github_link a').attr('href', github);
+ 		return false;
+ 	})
 	 
 })
